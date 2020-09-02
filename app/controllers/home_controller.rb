a531351752a8467ceedce647429cc544370fc99a
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-02 22:06:32 +0300

    Feature |BS-2| Bullet enable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   COUNT_LAST_BOOKS = 3
   def home
-    @books_slider = Book.last(COUNT_LAST_BOOKS)
+    @books_slider = Book.includes(:authors).last(COUNT_LAST_BOOKS)
   end
 end
