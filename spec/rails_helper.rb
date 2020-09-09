7ddfa2cc230cd5c4650d9553fae2fb665fcb728c
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-09 23:11:31 +0300

    Feature |BS-2| refactor rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
