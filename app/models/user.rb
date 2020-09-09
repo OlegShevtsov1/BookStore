bf62422ff1760621485fd43541ec4cc86c335a0f
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-09 09:53:00 +0300

    Feature |BS-4| configure validation User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  PASSWORD_FORMAT = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.freeze
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: PASSWORD_FORMAT }
+
+  before_validation :strip_password
+
+  def strip_password
+    self.password = password.strip
+    self.password_confirmation = password_confirmation.strip
+  end
 end
