198539c95079e17335e366011852f9d09d1e7d54
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-19 03:09:19 +0300

    Feature |BS-4| fix password validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
-  validates_with PasswordValidator
-  validates_with EmailValidator, if: :email.present?
+  validates_with PasswordValidator, EmailValidator
 
-  before_validation :strip_password
   validates :email, presence: true
 
   def self.from_omniauth(auth)
@@ -14,10 +12,5 @@ class User < ApplicationRecord
       user.skip_confirmation!
       user.password = Devise.friendly_token[0, 20]
     end
-  end
-
-  def strip_password
-    self.password = password.strip
-    self.password_confirmation = password_confirmation.strip if password_confirmation.present?
   end
 end
