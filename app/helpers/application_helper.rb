84848b65c37a7c4641bdf7bd5401924a4a4c04a9
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-19 03:57:17 +0300

    Feature |BS-4| add flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(key)
+    case key
+    when 'notice' then 'alert alert-success'
+    when 'alert' then 'alert alert-danger'
+    else
+      ''
+    end
+  end
 end
