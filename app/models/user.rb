66e686b151629cefc5631437cfca7db5ae144ea8
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-20 14:39:08 +0300

    Feature |BS-4| fix validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
-  validates_with PasswordValidator, EmailValidator
+  validates_with PasswordValidator, EmailValidator, on: :create
 
   validates :email, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -105,4 +105,7 @@ if BookAuthor.count.zero?
   BookAuthor.create!(book_id: 7, author_id: 2)
 end
 
-User.create!(email: 'john@dow.com', password: 'As12345678', confirmed_at: Time.current) if User.count.zero?
+if User.count.zero?
+  User.create!(email: 'john@dow.com', password: 'As12345678', confirmed_at: Time.current)
+  User.create!(email: 'john2@dow.com', password: 'As12345678', confirmed_at: Time.current)
+end
