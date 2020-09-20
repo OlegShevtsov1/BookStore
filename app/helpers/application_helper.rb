ec69b126c6cfb84985b77d0931c9750ebbe2aeb2
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-20 19:36:57 +0300

    Feature |BS-4| move flash notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def flash_class(key)
-    case key
-    when 'notice' then 'alert alert-success'
-    when 'alert' then 'alert alert-danger'
-    else
-      ''
-    end
-  end
 end
