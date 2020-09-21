f3558ae666fa053822bd46fbcfe773aa42d704d2
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-21 14:37:30 +0300

    Feature |BS-6| configure activeadmin categories page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,29 +4,11 @@ ActiveAdmin.register_page 'Dashboard' do
   content title: proc { I18n.t('active_admin.dashboard') } do
     div class: 'blank_slate_container', id: 'dashboard_default_message' do
       span class: 'blank_slate' do
-        span I18n.t('active_admin.dashboard_welcome.welcome')
-        small I18n.t('active_admin.dashboard_welcome.call_to_action')
+        div I18n.t('admin.dashboard.users_count', count: User.count)
+        div I18n.t('admin.dashboard.authors_count', count: Author.count)
+        div I18n.t('admin.dashboard.categories_count', count: Category.count)
+        div I18n.t('admin.dashboard.books_count', count: Book.count)
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end
 end
