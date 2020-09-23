b2495bd5292058c121ab6e543d79fb8669262d50
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-23 17:10:13 +0300

    Feature |BS-8| add activeadmin form ImageBook

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,9 +1,18 @@
 ActiveAdmin.register Book do
   includes :category, :book_authors, :authors
-  permit_params :name, :description, :price, :material, :year, :heigth, :width, :depth, :category_id, author_ids: []
+  permit_params :name, :description, :price, :material, :year, :heigth, :width, :depth, :category_id,
+                book_images_attributes: %i[id book_id image], author_ids: []
 
   index do
     selectable_column
+
+    column :book_image do |book|
+      if book.book_images.includes([:book_images]).any?
+        image_tag book.book_images.includes([:book_images]).first.image_url, class: 'admin-image-book'
+      else
+        image_tag 'active_admin/default.png', class: 'admin-image-book'
+      end
+    end
 
     column :category
     column :authors
@@ -38,6 +47,14 @@ ActiveAdmin.register Book do
       row :heigth
       row :width
       row :depth
+
+      row :book_cover do |book|
+        if book.book_images.includes([:book_images]).any?
+          image_tag book.book_images.includes([:book_images]).first.image_url, class: 'admin-image-book'
+        else
+          image_tag 'active_admin/default.png', class: 'admin-image-book'
+        end
+      end
     end
   end
 
@@ -53,7 +70,7 @@ ActiveAdmin.register Book do
   filter :created_at
   filter :updated_at
 
-  form name: I18n.t('admin.book.new') do |f|
+  form html: { multipart: true }, name: I18n.t('admin.book.new') do |f|
     f.inputs do
       f.input :name
       f.input :category
@@ -65,6 +82,10 @@ ActiveAdmin.register Book do
       f.input :heigth
       f.input :width
       f.input :depth
+
+      f.fields_for :book_images do |img|
+        img.file_field :image, multiple: true, name: 'book_images[image][]'
+      end
     end
     actions
   end
