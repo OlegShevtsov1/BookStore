877ddd1b40868b227e23648f0e48c0d9fd11bf2b
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-09-23 05:12:07 +0300

    Feature |BS-7| Brakeman Format Validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,21 +2,13 @@ class Comment < ApplicationRecord
   TITLE_MAX_SIZE = 80
   REVIEW_MAX_SIZE = 500
 
-  VALIDATE_TITLE = %r{[A-Za-z0-9 -! -# -$ -% -& -' - *-+ -/ -= -? -_ -` -{ -} -~ -. -^]}.freeze
-  VALIDATE_REVIEW = %r{[A-Za-z0-9 -! -# -$ -% -& -' - *-+ -/ -= -? -_ -` -{ -} -~ -. -^]}.freeze
-
   belongs_to :user
   belongs_to :book
   enum status: { unprocessed: 0, approved: 1, rejected: 2 }
 
   validates :title, :review, presence: true
   validates :title, length: { maximum: TITLE_MAX_SIZE,
-                              message: I18n.t('validate.comment.wrong_size', size: TITLE_MAX_SIZE) },
-                    format: { with: VALIDATE_TITLE,
-                              message: I18n.t('validate.comment.comment_format') }
-
+                              message: I18n.t('validate.comment.wrong_size', size: TITLE_MAX_SIZE) }
   validates :review, length: { maximum: REVIEW_MAX_SIZE,
-                               message: I18n.t('validate.comment.wrong_size', size: REVIEW_MAX_SIZE) },
-                     format: { with: VALIDATE_TITLE,
-                               message: I18n.t('validate.comment.comment_format') }
+                               message: I18n.t('validate.comment.wrong_size', size: REVIEW_MAX_SIZE) }
 end
