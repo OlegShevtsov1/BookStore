2ab09e48e5ff96cc948af6579ad8f26a8cfa0e7d
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-10-07 16:31:00 +0300

    Feature |BS-9| partial desktop/_item_form

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,5 @@
 class CartsController < ApplicationController
-  # skip_before_action :verify_authenticity_token
-
   def index
-    # @books_presenter = BooksPresenter.new(params: params)
-    # @cart_presenter = CartPresenter.new(current_order: current_order)
-    # @cart_books = current_order&.books
+    @items = current_order&.books
   end
 end
