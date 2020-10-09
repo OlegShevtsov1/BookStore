c18d169dcefe7eb67d1a86ad6e91861c8660586c
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-10-09 10:25:09 +0300

    Feature |BS-9| fix bullet notification

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   def index
-    @books = current_order&.books
+    @books = current_order&.books&.includes([:category])
     @current_order = current_order
   end
 end
