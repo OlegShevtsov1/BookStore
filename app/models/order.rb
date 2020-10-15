df492857ed5032328cf673fe2ad92dab3aaad907
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-10-15 09:01:35 +0300

    Feature |BS-9| some fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,4 @@ class Order < ApplicationRecord
   has_many :books, through: :items
   belongs_to :coupon, optional: true
   belongs_to :user, optional: true
-  enum status: { cart: 0, address: 1, fill_delivery: 2, payment: 3, confirm: 4, complete: 5,
-                 in_delivery: 6, delivered: 7, canceled: 8 }
 end
