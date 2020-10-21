450aa64c5e862e85668c9c9b5df3d40552c5b162
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-10-21 15:30:14 +0300

    Feature |BS-9| validate item quantity

--- app/forms/item_form.rb
+++ app/forms/item_form.rb
@@ -5,6 +5,7 @@ class ItemForm
   include ActiveModel::Model
   attr_accessor :current_user, :current_item, :quantity
 
-  validates :quantity, presence: true, length: { maximum: MAX_COUNT_ITEMS }
+  validates :quantity, presence: true, length: { maximum: MAX_COUNT_ITEMS },
+                       numericality: { only_integer: true, greater_than: 0 }
   validates :current_item, presence: true
 end
