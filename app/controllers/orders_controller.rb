6e1e8f9d2dc7eadf8495ccd95773867e96ba638d
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-10-27 08:32:47 +0200

    Feature |BS-12| add orders list

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,5 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Sort::SortOrdersService.new(Order.all, params['sort_by']).call
+  end
 end
---
+++ app/services/sort/sort_orders_service.rb
@@ -0,0 +1,16 @@
+module Sort
+  class SortOrdersService
+    SORT_BY = %w[waiting_for_processing in_delivery delivered canceled].freeze
+
+    def initialize(orders, sort_by)
+      @orders = orders
+      @sort_by = sort_by
+    end
+
+    attr_reader :orders, :sort_by
+
+    def call
+      SORT_BY.include?(sort_by) ? orders.public_send(sort_by) : orders.waiting_for_processing
+    end
+  end
+end
